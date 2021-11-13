d0eb3f56c3faf352cc4804705eaa7e034516a8fa
구영모 <dudah754@naver.com>
2021-11-13 14:17:50 +0900

    UI 그리기

--- app/src/main/java/com/nepplus/loginlogicpractice/MainActivity.kt
+++ app/src/main/java/com/nepplus/loginlogicpractice/MainActivity.kt
@@ -2,10 +2,23 @@ package com.nepplus.loginlogicpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnLogin.setOnClickListener {
+
+
+
+        }
+
+
+
+
+
     }
 }
